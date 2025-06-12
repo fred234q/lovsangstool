f0ef8b32ecb472a3784693ae31c208e55830fe23
unknown <frederikfrom@protonmail.com>
2025-06-12 23:40:19 +0200

    Created scraper for lovsang.dk.

--- search/search.py
+++ search/search.py
@@ -1,8 +1,11 @@
 from bs4 import BeautifulSoup
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.firefox.options import Options
 import os
 import pathlib
 import requests
+import time
 
 query = input("Query: ")
 
@@ -26,4 +29,31 @@ def scrape_worshiptoday(query):
     
     return results
 
-scrape_worshiptoday(query)
+def scrape_lovsang(query):
+    url = "https://lovsang.dk/sange.php?all"
+
+    # https://stackoverflow.com/a/60627463
+    options = Options()
+    options.add_argument("--headless")
+    driver = webdriver.Firefox(options=options)
+    driver.get(url)
+
+    # Close pop-ups
+    driver.find_element(By.CSS_SELECTOR, "button.fc-button").click()
+    driver.find_element(By.CSS_SELECTOR, "button.button").click()
+    
+    # Input search query in input field
+    input_field = driver.find_element(By.ID, "title_filter")
+    input_field.send_keys(query)
+    # Wait for search results to update
+    time.sleep(0.5)
+
+    soup = BeautifulSoup(driver.page_source, "html.parser")
+    driver.close()
+
+    results = soup.find_all(class_="songinfo")
+
+    for result in results:
+        result.title = result.span.get_text()
+        id = result.parent["ref"]
+        result.link = "https://lovsang.dk/song/view.php?song_id=" + id
