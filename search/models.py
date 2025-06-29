788e82cd05f5a0d098e57344cc91b3512863bc49
unknown <frederikfrom@protonmail.com>
2025-06-29 16:06:11 +0200

    Added a main_version property to Song model.

--- search/models.py
+++ search/models.py
@@ -9,6 +9,7 @@ class Song(models.Model):
     url = models.URLField(unique=True)
     source = models.ForeignKey("Source", on_delete=models.CASCADE, related_name="songs")
     chordpro = models.FileField(unique=True)
+    main_version = models.ForeignKey("self", on_delete=models.SET_NULL, null=True, blank=True, related_name="versions")
 
     def __str__(self):
         return self.title
