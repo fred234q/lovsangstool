1aec6630546a1b4f940411beac660ab20a263aee
fred234q <frederikfrom@disroot.org>
2026-02-05 09:09:20 +0100

    Set database to postgres.

--- lovsangstool/settings.py
+++ lovsangstool/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 import os
 
 from dotenv import load_dotenv
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -87,10 +88,11 @@ WSGI_APPLICATION = 'lovsangstool.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        # Replace this value with your local database's connection string.
+        default='postgresql://postgres:postgres@localhost:5432/lovsangstool',
+        conn_max_age=600
+    )
 }
 
 
